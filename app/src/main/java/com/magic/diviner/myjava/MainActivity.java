a3d9934816243ec04efa28ba53907f056fe617e7
DivinerMagic <diviner@promote.cache-dns.local>
2018-05-09 04:44:14 -0700

    Initial commit

---
+++ app/src/main/java/com/magic/diviner/myjava/MainActivity.java
@@ -0,0 +1,26 @@
+package com.magic.diviner.myjava;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
+public class MainActivity extends AppCompatActivity {
+
+    @BindView(R.id.text)
+    TextView text;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+    }
+
+    @OnClick(R.id.text)
+    public void onViewClicked() {
+    }
+}
